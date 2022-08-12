73ab33aae89891af711862f028ca512582e4964b
Carlos Schuenck <carlosschuenck@hotmail.com>
2022-08-12 12:54:54 +0100

    Adding material ui dependecies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react';
 import './App.css';
 
-import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
+import {ApolloClient, gql, InMemoryCache, useQuery} from "@apollo/client";
+import {Box, Button, createTheme, Grid, Paper, styled, ThemeProvider} from "@mui/material";
 
 
 const client = new ApolloClient({
@@ -9,9 +10,40 @@ const client = new ApolloClient({
     cache: new InMemoryCache(),
 });
 
+
+interface User {
+    id: string;
+    name: string;
+    age: number;
+}
+
+const GET_ROCKET_INVENTORY = gql`
+  query GetUser {
+    users {
+        id
+        name
+        age
+        address {
+            id
+            number
+            street
+            zipCode
+        }
+        orders {
+            id
+            description
+            userId
+        }
+    }
+  }
+`;
+
 function App() {
 
-  function testing(){
+  function Testing(){
+      const { loading, data } = useQuery(GET_ROCKET_INVENTORY);
+      console.log("use query",data)
+
       client.query({
           query: gql`
               query GetUser {
@@ -38,15 +70,48 @@ function App() {
   }
 
     useEffect(() => {
-        testing();
+        Testing();
     }, []);
 
 
   return (
     <div className="App">
-        <h1>OLÁ </h1>
+        <h1>OLÁ APP</h1>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <ThemeProvider  theme={lightTheme}>
+                    <Box
+                        sx={{
+                            p: 2,
+                            bgcolor: 'background.default',
+                            display: 'grid',
+                            gridTemplateColumns: { md: '1fr 1fr' },
+                            gap: 2,
+                        }}
+                    >
+                        {[0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => (
+                            <Item key={elevation} elevation={elevation}>
+                                {`elevation=${elevation}`}
+                            </Item>
+                        ))}
+                    </Box>
+                </ThemeProvider>
+            </Grid>
+        </Grid>
+        <Button variant="contained"> Olá Mundo</Button>
     </div>
   );
 }
+
+const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 60,
+    lineHeight: '60px',
+}));
+
 
 export default App;
